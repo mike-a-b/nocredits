2268db8fbe20656ef9a57be214930d619c0f62be
Mike B <mikebocharov89@gmail.com>
2022-12-16 23:20:43 +0300

    add some frames

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,4 @@
+
 import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 
-function btn1onclick() {
-    document.getElementById('btn2').classList.add('unchecked');
-    document.getElementById('btn1').classList.remove('unchecked');
-}
-function btn2onclick() {
-    document.getElementById('btn1').classList.add('unchecked');
-    document.getElementById('btn2').classList.remove('unchecked');
-}
